1a5048c282c646adc41de0d127c523bce8a841ae
blendicavlad <blendicav@gmail.com>
2020-04-16 02:48:00 +0300

    Licenta - gateway gzip fix

--- src/test/kotlin/com/dataseer/app/DSHttpClientTest.kt
+++ src/test/kotlin/com/dataseer/app/DSHttpClientTest.kt
@@ -1,27 +1,60 @@
 package com.dataseer.app
 
-import com.dataseer.app.config.HttpClientConfig
-import com.dataseer.app.config.RestTemplateConfig
+import com.dataseer.app.controller.AuthController
+import com.dataseer.app.payload.LoginRequest
+import com.dataseer.app.service.SecureDataSetStorageService
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.*
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 
 
-@RunWith(SpringJUnit4ClassRunner::class)
-@ContextConfiguration(classes = [HttpClientConfig::class, RestTemplateConfig::class])
+//@ContextConfiguration(classes = [HttpClientConfig::class, RestTemplateConfig::class])
+@SpringBootTest
+@RunWith(SpringRunner::class)
 class DSHttpClientTest {
     @Autowired
     lateinit var restTemplate: RestTemplate
+    @Autowired
+    lateinit var dataSetStorageService: SecureDataSetStorageService
+    @Autowired
+    lateinit var authController: AuthController
 
     @Test
     fun postResponse() {
         val uri = "http://localhost:8000/api/core"
-        val result = restTemplate.postForEntity(uri,null,String::class.java)
-        println(result.body)
-        Assert.assertTrue(result.body!!.contains("test"))
+        val loginRequest : LoginRequest = LoginRequest("andrei@gmail.com", "parola")
+        authController.authenticateUser(loginRequest)
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.MULTIPART_FORM_DATA
+        val fileMap = LinkedMultiValueMap<String, String>()
+        val body = LinkedMultiValueMap<String, Any>()
+        val contentDisposition: ContentDisposition = ContentDisposition
+                .builder("text/csv")
+                .name("file")
+                .filename("file.csv")
+                .build()
+        fileMap.add(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString())
+        val fileEntity : HttpEntity<ByteArray> = HttpEntity(dataSetStorageService.retrieveDataSet(17).get().data!!, fileMap)
+        body.add("file", fileEntity)
+        val requestEntity: HttpEntity<MultiValueMap<String, Any>> = HttpEntity(body, headers)
+        try {
+            val response = restTemplate.exchange(
+                    uri,
+                    HttpMethod.POST,
+                    requestEntity,
+                    String::class.java)
+                    println(response.body)
+        } catch (e: HttpClientErrorException) {
+            e.printStackTrace()
+        }
+        Assert.assertTrue(true)
     }
 }
